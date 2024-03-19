28c4e4ff472786630c22f83ec47dad2ab555f188
Pratik Sharma <ppratiksharma.2000@gmail.com>
2024-03-19 10:35:42 +0530

    Update RootPage.jsx

--- client/src/extra/RootPage.jsx
+++ client/src/extra/RootPage.jsx
@@ -1,19 +1,32 @@
 export const RootPage = () => {
+  const handlefirstcrane = () => {
+    
+  };
+  const handlesecondcrane = () => {
+
+  };
+  const handlethirdcrane = () => {
+
+  };
+
+  
   return (
     <>
-      <div>
-        hi
-        <div className="crane1">
-          <div className="image"></div>
-          <div className="name"></div>
+      <div className="flex flex-col md:flex-row justify-center items-center p-4 ">
+        <div className="flex  items-center flex-col gap-8 " onClick={handlefirstcrane}>
+          <img className="w-[50%] " src="/images/crane1.jpg" alt="crane1" />
+
+        <h2 className="text-xl font-medium">crane1</h2> 
         </div>
-        <div className="crane2">
-          <div className="image"></div>
-          <div className="name"></div>
+        <div className="flex  items-center flex-col gap-8 mt-2" onClick={handlesecondcrane}>
+          <img className="w-[50%]" src="/images/crane2.jpg" alt="crane2" />
+
+         <h2 className="text-xl font-medium">crane2</h2> 
         </div>
-        <div className="crane3">
-          <div className="image"></div>
-          <div className="name"></div>
+        <div className="flex  items-center flex-col gap-8 mt-4" onClick={handlethirdcrane}>
+          <img className="w-[50%]" src="/images/crane3.png" alt="crane3" />
+
+         <h2 className="text-xl font-medium">crane3</h2> 
         </div>
       </div>
     </>
